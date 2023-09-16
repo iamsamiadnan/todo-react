c748a339ee9aebd6d3f9a2b74a34e83c48d4f8e4
iamsamiadnan <sami.git@aol.com>
2023-09-17 01:43:53 +0600

    feat: add react toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import TodoWrapper from './components/TodoWrapper/TodoWrapper'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -16,17 +19,27 @@ function App() {
 
       setTodos(prev => [...prev, todo])
       // console.log('Log: ', e.target.value)
+
+      toast.success(``, {autoClose: 2000});
     }
+
+    
   }
 
   console.log('Log: ', todos)
 
   return (
-    <div className='w-1/5 mx-auto mt-10'>
-      <div className='mb-6'><input className='w-full border border-blue-900 p-3 rounded' type="text" onKeyDown={(e) => handleAddTodo(e)}/></div>
+    <>
+      <ToastContainer />
 
-      <TodoWrapper todos={todos}/>
-    </div>
+      <div className='w-1/5 mx-auto mt-10'>
+        <div className='mb-6'><input className='w-full border border-blue-900 p-3 rounded' type="text" onKeyDown={(e) => handleAddTodo(e)}/></div>
+
+        <TodoWrapper todos={todos}/>
+      </div>
+
+    </>
+    
   )
 }
 
