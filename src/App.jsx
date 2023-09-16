03c7899322438908d7ebfd8d413f488d334bdc47
iamsamiadnan <sami.git@aol.com>
2023-09-16 19:43:47 +0600

    feat: add html and style

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <></>
+    <div className='w-1/5 mx-auto mt-10'>
+      <div className='mb-6'><input className='w-full border border-blue-900 p-3 rounded' type="text" /></div>
+
+      <div>
+        <ul>
+          <li className='flex text-white bg-blue-900 p-3 rounded'>
+            <div className='flex-1'><span>1. Lorem, ipsum dolor.</span></div>
+            <div className='flex-1 flex justify-end'><button>Delete</button></div>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
