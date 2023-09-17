b1d03e4af7ba49c646de4840859b08a33b52dcbd
iamsamiadnan <sami.git@aol.com>
2023-09-18 01:31:23 +0600

    feat(tag): add tag html interaction

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,12 +1,35 @@
 import PropTypes from 'prop-types';
+import { Checkbox } from 'tiny-ui';
+import { FaPlusSquare } from "react-icons/fa";
+import { useState, useRef, useEffect } from 'react';
 
 function TodoItem({todo, handleRemoveTodo}) {
   const {id, text} = todo
+  const [tagInput, setTagInput] = useState(false)
+  const [tags, setTags] = useState([])
+  const tagInputRef = useRef(null);
+  
+  useEffect(() => {
+    tagInputRef.current.focus()
+  }, [tagInput])
 
   return (
-    <li className='flex text-white bg-blue-900 p-3 rounded'>
-            <div className='flex-1'><input type="checkbox" /> <span>{text}</span></div>
-            <div className='flex-1 flex justify-end'><button onClick={() => handleRemoveTodo(id)}>Delete</button></div>
+    <li className=' bg-blue-900 p-3 rounded'>
+            <div className='flex text-white'>
+              <div className='flex-1'> <Checkbox>{text}</Checkbox></div>
+              <div className='flex-1 flex justify-end'><button onClick={() => handleRemoveTodo(id)}>Delete</button></div>
+            </div>
+            <div className='flex items-center gap-1'>
+              
+              
+
+              <input  type="text" className='text-[12px] w-12 rounded-sm' hidden={!tagInput} ref={tagInputRef} />
+             
+              <FaPlusSquare className='hover:cursor-pointer text-white' onClick={() => setTagInput(!tagInput)}/>
+            
+              
+            </div>
+       
     </li>
   )
 }
